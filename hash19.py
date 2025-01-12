6b9708dd8257b2f5943f158fde96b10e27498fb0
azizbek512 <azizbekr3399@gmail.com>
2025-01-12 09:28:50 +0500

    watch different word and hash

---
+++ hash19.py
@@ -0,0 +1,14 @@
+#  "Python!" va "Python " o‘rtasidagi hash farqini toping.
+import hashlib
+text1 = "Python!"
+text2 = "Python"
+hash1 = hashlib.sha256(text1.encode()).hexdigest()
+hash2 = hashlib.sha256(text2.encode()).hexdigest()
+if hash1 == hash2:
+    print("same!")
+else:
+    print("different!")
+print("text1: ", text1)
+print("hash1: ", hash1)
+print("text2: ", text2)
+print("hash2: ", hash2)
