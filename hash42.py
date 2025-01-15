0941342113398b178ec3e026af8a5af71b4243c4
azizbek512 <azizbekr3399@gmail.com>
2025-01-15 06:17:10 +0500

    get text from random and save hash results in list

--- hash42.py
+++ hash42.py
@@ -1,1 +1,18 @@
 # 5 ta tasodifiy matn hashini ro‘yxatda saqlang.
+import hashlib
+import os
+def randomtext(length=10):
+    random_bites = os.urandom(length)
+    return ''.join(chr(b) for b in range(length))
+
+hash_list = []
+
+for _ in range(5):
+    text = randomtext(10)
+    text_hash = hashlib.sha256(text.encode()).hexdigest()
+    hash_list.append((text, text_hash))
+
+for i, (text, hash_value) in enumerate(hash_list, 1):
+    print(f"{i}-matn: {text}\nhash: {hash_value}\n")
+    
+print("list: ", hash_list)
